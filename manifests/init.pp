8a486dacd0e7bde236a8a28a1b4af5805ce0c4b0
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-09-05 10:32:07 +0200

    moved stuff for frank

---
+++ manifests/init.pp
@@ -0,0 +1,55 @@
+# kibana / nginx is a graphical frontend to elasticsearch.
+# nginx is installed and a debian package containing the
+# webroot.
+
+class kibana::install {
+    package { "nginx":
+        ensure => present,
+    }
+
+    package { "php5-fpm":
+        ensure => present,
+        require => Package["nginx"],
+    }
+
+    package { "kibana":
+        ensure => latest,
+        require => Package["php5-fpm"],
+        notify => Class["kibana::config"]
+    }
+}
+
+class kibana::config {
+    file { "/etc/nginx/sites-available/default":
+        source => "puppet:///modules/kibana/default",
+        group => "root",
+        owner => "root",
+        mode => "0644",
+        ensure => file,
+        replace => true,
+        require => Class["kibana::install"],
+        notify => Class["kibana::service"],
+    }
+}
+
+class kibana::service { 
+    service { "nginx":
+        ensure => running,
+        enable => true, 
+        hasrestart => true,
+        require => File["/etc/nginx/sites-available/default"]
+    }
+
+    service { "php5-fpm":
+        ensure => running,
+        enable => true, 
+        hasrestart => true,
+    }
+}
+
+class kibana {
+    include kibana::install
+    include kibana::config
+    include kibana::service
+}
+
